b947bd3c9be349298756ac40d088f1dd4c28ea67
0rc1nus <mitchellctlui@gmail.com>
2023-04-28 10:08:46 +0800

    Added loot tables

---
+++ src/main/java/com/ninni/twigs/mixin/LootItemAccessor.java
@@ -0,0 +1,17 @@
+package com.ninni.twigs.mixin;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LootItem.class)
+public interface LootItemAccessor {
+    @Accessor
+    Item getItem();
+
+    @Mutable
+    @Accessor
+    void setItem(Item item);
+}
