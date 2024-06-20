f5bc83bd2384c61adbd0dadc2333db764855be91
theendercore <theendercore@gmail.com>
2024-06-20 21:54:29 +0300

    - changed the tag to not hurt my soul

--- src/main/java/com/ninni/twigs/TwigsTags.java
+++ src/main/java/com/ninni/twigs/TwigsTags.java
@@ -1,6 +1,8 @@
 package com.ninni.twigs;
 
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
@@ -13,33 +15,37 @@ import static com.ninni.twigs.Twigs.MOD_ID;
 @SuppressWarnings("unused")
 public interface TwigsTags {
 
-    //item tags
-    TagKey<Item> SCHIST_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "schist"));
-    TagKey<Item> RHYOLITE_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "rhyolite"));
-    TagKey<Item> BLOODSTONE_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "bloodstone"));
-    TagKey<Item> PACKED_SILT_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "packed_silt"));
-    TagKey<Item> PACKED_SILT_FULL_BLOCKS = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "packed_silt_full_blocks"));
-    TagKey<Item> SILT_POTS_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "silt_pots"));
-    TagKey<Item> SEASHELLS = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "seashells"));
-    TagKey<Item> TABLES_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "tables"));
+	//item tags
+	TagKey<Item> SCHIST_ITEM = createTag(Registries.ITEM, "schist");
+	TagKey<Item> RHYOLITE_ITEM = createTag(Registries.ITEM, "rhyolite");
+	TagKey<Item> BLOODSTONE_ITEM = createTag(Registries.ITEM, "bloodstone");
+	TagKey<Item> PACKED_SILT_ITEM = createTag(Registries.ITEM, "packed_silt");
+	TagKey<Item> PACKED_SILT_FULL_BLOCKS = createTag(Registries.ITEM, "packed_silt_full_blocks");
+	TagKey<Item> SILT_POTS_ITEM = createTag(Registries.ITEM, "silt_pots");
+	TagKey<Item> SEASHELLS = createTag(Registries.ITEM, "seashells");
+	TagKey<Item> TABLES_ITEM = createTag(Registries.ITEM, "tables");
 
-    //block tags
-    TagKey<Block> SCHIST_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "schist"));
-    TagKey<Block> RHYOLITE_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "rhyolite"));
-    TagKey<Block> BLOODSTONE_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "bloodstone"));
-    TagKey<Block> PACKED_SILT_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "packed_silt"));
-    TagKey<Block> SILT_POTS_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "silt_pots"));
-    TagKey<Block> OFFSET_REMOVER = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "offset_remover"));
-    TagKey<Block> TABLES_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "tables"));
+	//block tags
+	TagKey<Block> SCHIST_BLOCK = createTag(Registries.BLOCK, "schist");
+	TagKey<Block> RHYOLITE_BLOCK = createTag(Registries.BLOCK, "rhyolite");
+	TagKey<Block> BLOODSTONE_BLOCK = createTag(Registries.BLOCK, "bloodstone");
+	TagKey<Block> PACKED_SILT_BLOCK = createTag(Registries.BLOCK, "packed_silt");
+	TagKey<Block> SILT_POTS_BLOCK = createTag(Registries.BLOCK, "silt_pots");
+	TagKey<Block> OFFSET_REMOVER = createTag(Registries.BLOCK, "offset_remover");
+	TagKey<Block> TABLES_BLOCK = createTag(Registries.BLOCK, "tables");
 
-    //biome tags
-    TagKey<Biome> BLOODSTONE_GENERATES = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "bloodstone_generates"));
-    TagKey<Biome> SCHIST_GENERATES = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "schist_generates"));
-    TagKey<Biome> SILT_GENERATES = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "silt_generates"));
-    TagKey<Biome> AZALEA_FLOWERS_GENERATE = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "azalea_flowers_generate"));
-    TagKey<Biome> SPAWNS_TWIG = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "spawns_twig"));
-    TagKey<Biome> SPAWNS_PEBBLE = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "spawns_pebble"));
-    TagKey<Biome> SPAWNS_SEA_SHELL = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "spawns_sea_shell"));
+	//biome tags
+	TagKey<Biome> BLOODSTONE_GENERATES = createTag(Registries.BIOME, "bloodstone_generates");
+	TagKey<Biome> SCHIST_GENERATES = createTag(Registries.BIOME, "schist_generates");
+	TagKey<Biome> SILT_GENERATES = createTag(Registries.BIOME, "silt_generates");
+	TagKey<Biome> AZALEA_FLOWERS_GENERATE = createTag(Registries.BIOME, "azalea_flowers_generate");
+	TagKey<Biome> SPAWNS_TWIG = createTag(Registries.BIOME, "spawns_twig");
+	TagKey<Biome> SPAWNS_PEBBLE = createTag(Registries.BIOME, "spawns_pebble");
+	TagKey<Biome> SPAWNS_SEA_SHELL = createTag(Registries.BIOME, "spawns_sea_shell");
 
-    TagKey<EntityType<?>> BAMBOO_LEAVES_SLOW_IMMUNE = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(MOD_ID, "bamboo_leaves_slow_immune"));
+	TagKey<EntityType<?>> BAMBOO_LEAVES_SLOW_IMMUNE = createTag(Registries.ENTITY_TYPE, "bamboo_leaves_slow_immune");
+
+	static <T> TagKey<T> createTag(ResourceKey<Registry<T>> resourceKey, String path) {
+		return TagKey.create(resourceKey, ResourceLocation.fromNamespaceAndPath(MOD_ID, path));
+	}
 }
